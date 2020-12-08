cd3a0d881af6c207d97356a3123ef80cc0d7d7a3
Amine L'ch <aminelch95@gmail.com>
2020-12-08 23:28:58 +0100

    listing of softwares

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SoftwareRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(SoftwareRepository $repository): Response
     {
+        $items = $repository->findBy([], ["id"=>"DESC"]);
+        dump($items);
         return $this->render('home/index.html.twig', [
-
+         "softwareList"=>$items
         ]);
     }
 }
