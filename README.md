81b546ec61f90b9da858b1261c97fba835ae3781
Amine L'ch <aminelch95@gmail.com>
2020-12-08 22:53:39 +0100

    creating a software entity, migrate the database

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# Welcome to StackEdit!
-
-
 
 # Test Web dev
 
   #### How it works
 
+ ```bash
     $- git clone https://github.com/aminelch/test-web-dev.git
     $- cd test-web-dev
     $- composer install
     $- symfony serve -d
 
+##### environement
+- PHPStorm
+- Firefox
+- Ubuntu 20.4
---
+++ src/Entity/Software.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SoftwareRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SoftwareRepository::class)
+ */
+class Software
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $constructor;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getConstructor(): ?string
+    {
+        return $this->constructor;
+    }
+
+    public function setConstructor(string $constructor): self
+    {
+        $this->constructor = $constructor;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
