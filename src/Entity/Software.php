4992393870e4334287e19920fa642920e173cf6d
Amine L'ch <aminelch95@gmail.com>
2020-12-08 23:40:34 +0100

    styling the listing page, showing 4 items per row, adding description field and migrate the databse

--- src/Entity/Software.php
+++ src/Entity/Software.php
@@ -32,6 +32,11 @@ class Software
      */
     private $category;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -69,6 +74,18 @@ class Software
     public function setCategory(string $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
